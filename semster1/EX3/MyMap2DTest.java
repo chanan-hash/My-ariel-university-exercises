9d8e363a4b64cf79597f15147703a1a6c4613c7b
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2022-12-04 23:43:16 +0200

    First test for rectangle

--- semster1/EX3/MyMap2DTest.java
+++ semster1/EX3/MyMap2DTest.java
@@ -1,17 +1,47 @@
 package Exe.EX3;
 
 import static org.junit.Assert.*;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class MyMap2DTest {
+	
+	private static  Map2D _map_test = null;
 	
 	@Test
 	/**
 	 * This test for checking the rectanlge_draw function
 	 */
+	
 	public void testRect() {
+		Point2D p1 = new Point2D(1,2);
+		Point2D p2 = new Point2D(4,10);
+		int minX = Math.min(p1.ix(), p2.ix());
+		int minY = Math.min(p1.iy(), p2.iy());
 		
+		int width = Math.abs((p1.ix() - p2.ix())); // the width of the
+		int hight = Math.abs((p1.iy() - p2.iy()));
+		_map_test.init(width, hight);
+		_map_test.drawRect(p1, p2, 1);
+		
+		int[][] tmp = new int[_map_test.getHeight()][_map_test.getWidth()];
+		for (int i = 0; i < _map_test.getHeight(); i++) {
+			for (int j = 0; j < _map_test.getWidth(); j++)  {
+				tmp[i][j] = _map_test.getPixel(i, j);  // deep copy for the color num
+			}
+		}
+		for (int i = 0; i <= hight; i++) {
+			for (int j = 0; j <= width; j++) {
+				System.out.println(tmp[i][j]);
+				assertEquals(tmp[j + minX][i + minY] , 1);
+			
+			}
+		}
+	
+	
 	}
-
+	
+	
+	
+	
 }
